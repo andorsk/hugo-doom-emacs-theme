e6ed4f23ba0de1bc320d4e23198820cf2819a293
Andor Kesselman <andor@henosisknot.com>
2022-11-24 07:18:26 +0530

    good progress. shortcuts are working

--- static/js/index.js
+++ static/js/index.js
@@ -9,3 +9,30 @@ function toggleBuffer() {
 function sendMessage(msg) {
   document.getElementById("message-buffer").innerText = msg;
 }
+
+document.addEventListener("keydown", (event) => {
+  switch (event.code) {
+    case "KeyB":
+      toggleBuffer();
+      return;
+    case "KeyH":
+      toggleHelpModal();
+      return;
+    case "KeyM":
+      GoHome();
+      return;
+  }
+});
+
+function toggleHelpModal() {
+  var x = document.getElementById("helpModal");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function GoHome() {
+  window.location.href = "/";
+}
