ea38ee7e10283dd837c9b5cd94e2f3c8944f1dfc
Andor Kesselman <andor@henosisknot.com>
2022-11-26 02:06:19 +0530

    fix scrolling`

--- static/js/index.js
+++ static/js/index.js
@@ -10,26 +10,53 @@ function sendMessage(msg) {
   document.getElementById("message-buffer").innerText = msg;
 }
 
+const moveRate = 100;
+const moveRatePage = window.outerHeight;
+
 document.addEventListener("keydown", (event) => {
   console.log(event);
-  switch (event.code) {
-    case "KeyB":
+  switch (event.key) {
+    case "b":
       toggleBuffer();
       return;
-    case "KeyH":
+    case "h":
       toggleHelpModal();
       return;
-    case "Slash":
+    case "?":
       toggleHelpModal();
       return;
-    case "KeyP":
+    case "p":
       redirect("/posts");
       return;
-    case "KeyT":
+    case "t":
       redirect("/tags");
       return;
-    case "KeyM":
+    case "m":
       redirect("/");
+      return;
+    case "j":
+      moveDown(moveRate);
+      return;
+    case "k":
+      moveUp(moveRate);
+      return;
+    case "g":
+      moveToTop();
+      return;
+    case "d":
+      if (event.ctrlKey === true) {
+        console.log("page down");
+        moveDown(moveRatePage / 2);
+        return;
+      }
+    case "u":
+      if (event.ctrlKey === true) {
+        console.log("page up");
+        moveUp(moveRatePage / 2);
+      }
+      return;
+    case "G":
+      moveToBottom();
       return;
   }
 });
@@ -45,4 +72,20 @@ function toggleHelpModal() {
 
 function redirect(path) {
   window.location.href = "{{ .Site.BaseURL }}" + path;
+}
+
+function moveDown(x) {
+  window.scrollBy(0, x);
+}
+
+function moveUp(x) {
+  window.scrollBy(0, -1 * x);
+}
+
+function moveToTop() {
+  scrollTo(0, 0);
+}
+
+function moveToBottom() {
+  scrollTo(0, document.body.scrollHeight);
 }
